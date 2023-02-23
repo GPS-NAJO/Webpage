01377ecf7c5b2cb6dd276a3712118de73301af84
A-FlorezMeyer <116691882+A-FlorezMeyer@users.noreply.github.com>
2023-02-23 00:36:44 -0500

    version 1-2

--- main.js
+++ main.js
@@ -4,23 +4,39 @@ const listener = dgram.createSocket('udp4');
 const fs = require('fs');
 const path = require('path');
 
-var mensaje = '1;1;1;1'
+var mensaje = '1;1;1;1' // Inicio de mensaje
+
+
+/*
+
+Inicia el listener que escucha para los datos udp provenientes de la app
+
+*/
 listener.on('listening', () => {
     const address = listener.address();
     console.log(`El listener UDP está escuchando en ${address.address}:${address.port}`);
   });
 
+
+
+  /*
+
+  Imprime mensaje en consola para efectos de depuracion
+
+*/
   listener.on('message', (msg, rinfo) => {
     console.log(`Mensaje recibido desde ${rinfo.address}:${rinfo.port}: ${msg}`);
     datogps(msg);
   });
 
-  listener.bind(52022,'192.168.1.16');
+  listener.bind(52022,'192.168.1.16'); // Vincula el puerto e ip
 
 
   const server = http.createServer(async (req, res) => {
+
+
     if (req.url === '/') {
-      console.log('Cargando archivo HTML')
+
       const htmlPath = path.join(__dirname, 'index.html');
       const fileStream = fs.createReadStream(htmlPath);
     fileStream.on('error', (err) => {
@@ -30,36 +46,13 @@ listener.on('listening', () => {
     });
     res.writeHead(200, { 'Content-Type': 'text/html' });
     fileStream.pipe(res);
-/*
-      fs.readFile(htmlPath,'UTF-8', (err, data) => {
-        console.log('hoola')
-        if (err) {
-          console.log('error')
-          res.writeHead(500);
-          res.end('Error interno del servidor');
-        } else {
-          console.log('success')
-          res.writeHead(200, { 'Content-Type': 'text/html' });
-          res.end(data);
-        }
-      });
 
-      */
+
     } else if (req.url === '/estilos.css'){
+
       const cssPath = path.join(__dirname, 'estilos.css');
-/*
-    fs.readFile(cssPath,'UTF-8', (err, data) => {
-      console.log('cargando css')
-      if (err) {
-        res.writeHead(500);
-        res.end('Error interno del servidor');
-      } else {
-        res.writeHead(200, { 'Content-Type': 'text/css' });
-        res.end(data);
-      }
-    });
-*/
-const fileStream = fs.createReadStream(cssPath);
+
+      const fileStream = fs.createReadStream(cssPath);
     fileStream.on('error', (err) => {
       console.log('Error al leer archivo CSS:', err);
       res.writeHead(500);
@@ -67,15 +60,16 @@ const fileStream = fs.createReadStream(cssPath);
     });
     res.writeHead(200, { 'Content-Type': 'text/css' });
     fileStream.pipe(res);
+
     }else if (req.url === '/api/valor'){
-      console.log('cargando dato');
+
       if(mensaje != null){
         var gpsdata = mensaje.split(';');
         var gpsjson = {
           latitud: gpsdata[0],
           longitud: gpsdata[1],
           altitud: gpsdata[2],
-          timestamp: gpsdata[3]
+          timestamp: parseInt(gpsdata[3],10)
         };
         res.setHeader('Content-Type', 'application/json');
         res.end(JSON.stringify(gpsjson));
