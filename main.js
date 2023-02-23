200765fbde170d3aadd8cc1d85273eddc73fc168
A-FlorezMeyer <116691882+A-FlorezMeyer@users.noreply.github.com>
2023-02-22 23:29:37 -0500

    por ahora funciona

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 const http = require('http');
 const dgram = require('dgram');
 const listener = dgram.createSocket('udp4');
-const fs = require('fs/promises');
+const fs = require('fs');
 const path = require('path');
 
-var mensaje = 'Hola'
+var mensaje = '1;1;1;1'
 listener.on('listening', () => {
     const address = listener.address();
     console.log(`El listener UDP está escuchando en ${address.address}:${address.port}`);
@@ -22,6 +22,15 @@ listener.on('listening', () => {
     if (req.url === '/') {
       console.log('Cargando archivo HTML')
       const htmlPath = path.join(__dirname, 'index.html');
+      const fileStream = fs.createReadStream(htmlPath);
+    fileStream.on('error', (err) => {
+      console.log('Error al leer archivo HTML:', err);
+      res.writeHead(500);
+      res.end('Error interno del servidor');
+    });
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    fileStream.pipe(res);
+/*
       fs.readFile(htmlPath,'UTF-8', (err, data) => {
         console.log('hoola')
         if (err) {
@@ -34,8 +43,11 @@ listener.on('listening', () => {
           res.end(data);
         }
       });
+
+      */
     } else if (req.url === '/estilos.css'){
       const cssPath = path.join(__dirname, 'estilos.css');
+/*
     fs.readFile(cssPath,'UTF-8', (err, data) => {
       console.log('cargando css')
       if (err) {
@@ -46,6 +58,15 @@ listener.on('listening', () => {
         res.end(data);
       }
     });
+*/
+const fileStream = fs.createReadStream(cssPath);
+    fileStream.on('error', (err) => {
+      console.log('Error al leer archivo CSS:', err);
+      res.writeHead(500);
+      res.end('Error interno del servidor');
+    });
+    res.writeHead(200, { 'Content-Type': 'text/css' });
+    fileStream.pipe(res);
     }else if (req.url === '/api/valor'){
       console.log('cargando dato');
       if(mensaje != null){
@@ -67,9 +88,9 @@ server.listen(51012,'192.168.1.16', () => {
 });
 
 async function datogps(msg) {
-    mensaje = await msg;
+    mensaje = await msg.toString('utf8');
     try {
-        await fs.appendFile('historial.txt',mensaje + "\n");
+        await fs.promises.appendFile('historial.txt',mensaje + "\n");
     } catch (err){
         console.log(err);
     }
