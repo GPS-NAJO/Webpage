92bf603fd32d5579dd8691f7da04230d48ec08ae
A-FlorezMeyer <116691882+A-FlorezMeyer@users.noreply.github.com>
2023-03-06 15:38:44 -0500

    a1

--- listener/index.js
+++ listener/index.js
@@ -1,6 +1,6 @@
 const dgram = require("dgram");
 const { Message } = require("./message.js");
-const Database = require("../config.js").Database;
+const Database = require("../config.js");
 const listener = dgram.createSocket("udp4");
 
 const message = new Message();
