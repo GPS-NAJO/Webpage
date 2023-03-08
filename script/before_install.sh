574e26b45785f349ee13f53c8003bff40a7466f0
Orlando Barboza <124634329+B4rz99@users.noreply.github.com>
2023-03-08 01:43:15 -0500

    Update before_install.sh

--- script/before_install.sh
+++ script/before_install.sh
@@ -4,8 +4,9 @@ PID=$(pgrep -f "node bin/www")
  if [ -n "$PID" ]; then
       sudo kill "$PID"
  fi
+cd /home/ubuntu/webpage/Webpage
 sudo apt install npm -y
 sudo apt install nodejs -y
 sudo apt install mysql-client -y
-cd /home/ubuntu/webpage/Webpage
+git pull
 sudo npm install
