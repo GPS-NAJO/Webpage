2d5316660aa910b87a5769982e7e4e3012390715
A-FlorezMeyer <116691882+A-FlorezMeyer@users.noreply.github.com>
2023-03-09 21:58:14 -0500

    cambio

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -53,6 +53,11 @@ function getData() {
           maxZoom: 20,
         }).addTo(map);
         // Crea un control de rutas y agrega la ruta al mapa
+        
+
+        marker = L.marker(coordenadas[coordenadas.length - 1]).addTo(nav);
+        status = 1;
+      } else {
         if (coordenadas.length > 1) {
           var polyline = L.polyline(
             [
@@ -64,10 +69,6 @@ function getData() {
             }
           ).addTo(map);
         }
-
-        marker = L.marker(coordenadas[coordenadas.length - 1]).addTo(nav);
-        status = 1;
-      } else {
         nav.setView(coords, 18);
         marker?.setLatLng(coordenadas[coordenadas.length - 1]);
       }
