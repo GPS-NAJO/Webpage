30413fc04b1b3b9aac32cbeeb7aa4942818d5095
A-FlorezMeyer <116691882+A-FlorezMeyer@users.noreply.github.com>
2023-03-09 22:32:16 -0500

    cambio yaml

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+sudo git pull
+sudo npm install
 sudo nohup node bin/www &
--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 sudo cd /home/ubuntu/Webpage
-sudo git pull
 PID=$(pgrep -f "node bin/www")
  if [ -n "$PID" ]; then
       sudo kill "$PID"
