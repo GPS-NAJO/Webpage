d943448f04d67cb46cce674e099c8afa31f57ff4
Juan Sebastián Camargo Jiménez <jimenezsj@uninorte.edu.co>
2023-03-10 22:30:53 -0500

    Despliegue de pagina para el historial de rutas boton para regresar a la pagina principal

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var udplistener = require('./listener/index')
 var homeRouter = require('./routes/home');
-var histoRouter = require('./routes/historial')
+
 var app = express();
 var {message} = require('./listener/index.js');
 //probandopipeline
